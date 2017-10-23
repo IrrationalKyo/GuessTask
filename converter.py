7401acf2c0fd33c0305cd04719028081b9fece89
Rick <rick@localhost.localdomain>
2017-10-22 21:58:19 -0500

    re-adjusting the shape of the input and the indices

--- converter.py
+++ converter.py
@@ -20,30 +20,45 @@ def text_to_list(fileName):
 			time_range = None
 	return trace
 
-''' converts the task list to numpy of shape (data_size, feature_size)'''
-def list_to_array(trace_list, training_ratio, feature_size, label_card, label_size=1):
-	data_size = len(trace_list)-(feature_size+label_size)
+'''
+	converts the task list to numpy of shape (data_size, feature_size)
+	
+	label_card := cardinality of the label set
+	label_size := number of slot predictions
+'''
+def list_to_array(trace_list, training_ratio, time_steps, label_card, label_size=1):
+
+	# data_size := total number of data points that can be generated
+	data_size = math.floor(len(trace_list)/(time_steps + label_size))
+	# training_size := number of training samples
 	training_size = int(data_size * training_ratio)
+	# test_size := number of test samples
 	test_size = data_size - training_size
-	training_examples = np.zeros((training_size, 1, feature_size))
-	training_labels = np.zeros(( training_size, 1,label_card))
-	test_examples = np.zeros((test_size, 1, feature_size))
-	test_labels = np.zeros((test_size,1,label_card))
+
+
+	training_examples = np.zeros((training_size, time_steps, label_card))
+	training_labels = np.zeros((training_size, time_steps, label_card))
+
+	test_examples = np.zeros((test_size, time_steps, label_card))
+	test_labels = np.zeros((test_size, time_steps, label_card))
 	
 	for i in range(data_size):
-		if data_size - i < feature_size + label_size:
-			break
+		# if data_size - i < time_steps + label_size:
+		# 	break
 		if i < training_size:
-			for j in range(feature_size):
-				training_examples[i][0][j] = trace_list[i+j]
-			for j in range(label_size):
-				label = trace_list[i+feature_size+j]
-				training_labels[i][0][label] = 1
+			for j in range(time_steps):
+				''' TODO: find the correct index'''
+				task_num = trace_list[i + j]
+				training_examples[i][j][task_num] = 1
+				for k in range(label_size):
+					''' TODO: find the correct index'''
+					label = trace_list[i + time_steps + j]
+					training_labels[i][j][label] = 1
 		else:
-			for j in range(feature_size):
+			for j in range(time_steps):
 				test_examples[i-training_size][0][j] = trace_list[i+j]
 			for j in range(label_size):
-				label = trace_list[i+feature_size+j]
+				label = trace_list[i + time_steps + j]
 				test_labels[i-training_size][0][label] = 1
 	return (training_examples, training_labels), (test_examples,test_labels)
 	
