f7efdde714d0176f828cfab73e5c02dcaa806fb7
Kyo Kim <kyokim751@gmail.com>
2017-11-16 09:33:20 -0600

    updating

--- converter.py
+++ converter.py
@@ -20,85 +20,186 @@ def text_to_list(fileName):
 			time_range = None
 	return trace
 
+def list_to_example(trace_list, label_card, time_steps, label_size=1, offset=0):
+	list_of_examples = []
+	list_of_labels = []
+	for i in range(len(trace_list)):
+		if i + time_steps+offset+label_size >= len(trace_list):
+			break
+
+		# squash the mutiple steps
+		example_datum = np.zeros((time_steps,label_card))
+		for j in range(time_steps):
+			task_num = trace_list[j+i]
+			example_datum[j][int(task_num)] = 1
+		list_of_examples.append(example_datum)
+
+		label_datum = np.zeros((label_size, label_card))
+		for j in range(label_size):
+			task_num = trace_list[i + time_steps + offset + j]
+			label_datum[j][int(task_num)] = 1
+
+		list_of_labels.append(label_datum)
+
+	return (list_of_examples, list_of_labels)
+
+
+
 '''
 	converts the task list to numpy of shape (data_size, feature_size)
 	
 	label_card := cardinality of the label set
 	label_size := number of slot predictions
+	offset := number of slots ahead of the next slot
 '''
-def list_to_array(trace_list, training_ratio, time_steps, label_card, label_size=1):
-	# data_size := total number of data points that can be generated
-	data_size = len(trace_list) - (time_steps + label_size)
-	# training_size := number of training samples
-	training_size = int(data_size * training_ratio)
-	# test_size := number of test samples
-	test_size = data_size - training_size
-
-	# training_examples = np.zeros((training_size, time_steps, label_card))
-	# training_labels = np.zeros((training_size, time_steps, label_card))
-
-	# test_examples = np.zeros((test_size, time_steps, label_card))
-	# test_labels = np.zeros((test_size, time_steps, label_card))
-
-	training_examples, training_labels = sample_creater(trace_list[:training_size + time_steps +label_size],
-														training_size,
-														time_steps,
-														label_card)
-	test_examples, test_labels = sample_creater(trace_list[training_size:],
-												  test_size,
-												  time_steps,
-												  label_card)
-
-	return (training_examples, training_labels), (test_examples, test_labels)
-
-
-'''
-	Creates samples so each new example advances by 1 timestep (1 new label example)
-'''
-def sample_creater(trace_list, count, time_steps, label_card, label_size = 1):
-	if count + time_steps + label_size > len(trace_list):
-		raise ValueError("trace_list is too short."
-						 "\tcount = " + str(count) +
-						 "\tlabel_size =" + str(label_size) +
-						 "\tlen(trace_list)" + str(len(trace_list)))
-	if time_steps >= len(trace_list):
-		raise ValueError("time_steps is larger than trace_list.\ntrace_list is too short"
-			  "\ttime_steps = " + str(time_steps) +
-			  "\tlen(trace_list)" + str(len(trace_list)))
-
-	example = np.zeros((count, time_steps, label_card))
-	label = np.zeros((count, time_steps, label_card))
-
-	for l in range(count):
-		for j in range(time_steps):
-			''' TODO: find the correct index'''
-			task_num = trace_list[l + j]
-			example[l][j][task_num] = 1
-			for k in range(1, label_size + 1):
-				''' TODO: find the correct index'''
-				task_num = trace_list[l + j + k]
-				label[l][j][task_num] = 1
-	return example, label
-
-'''
-	requires the length of the trace_list to be divisible by count.
-'''
-def sample_creater_disjoint(trace_list, count, time_steps, label_card, label_size = 1):
-
-	example_len = (label_size + time_steps)
-
-	example = np.zeros((count, time_steps, label_card))
-	label = np.zeros((count, time_steps, label_card))
-
-	for i in range(count):
-		for j in range(time_steps):
-			task_num = trace_list[i*(example_len) + j]
-			example[i][j][task_num] = 1
-			for k in range(1,1+label_size):
-				label_num = trace_list[i*(example_len) + j + k]
-				label[i][j][label_num] = 1
-
-	return example, label
+# def list_to_array(trace_list, training_ratio, time_steps, label_card, label_size=1, offset = 0):
+# 	# data_size := total number of data points that can be generated
+# 	data_size = len(trace_list) - (time_steps + label_size)
+# 	# training_size := number of training samples
+# 	training_size = int(data_size * training_ratio)
+# 	# test_size := number of test samples
+# 	test_size = data_size - training_size
+#
+# 	# training_examples = np.zeros((training_size, time_steps, label_card))
+# 	# training_labels = np.zeros((training_size, time_steps, label_card))
+#
+# 	# test_examples = np.zeros((test_size, time_steps, label_card))
+# 	# test_labels = np.zeros((test_size, time_steps, label_card))
+#
+# 	training_examples, training_labels = sample_creater(trace_list[:training_size + time_steps +label_size],
+# 														training_size,
+# 														time_steps,
+# 														label_card,
+# 														offset=offset)
+# 	test_examples, test_labels = sample_creater(trace_list[training_size:],
+# 												test_size,
+# 												time_steps,
+# 												label_card,
+# 												offset=offset)
+#
+# 	return (training_examples, training_labels), (test_examples, test_labels)
 
 
-(train_x2, train_y2), (test_x2,test_y2) = list_to_array(text_to_list('dataset_1.txt'), 0.8, 100, 16)
+# '''
+# 	Creates samples so each new example advances by 1 timestep (1 new label example)
+# '''
+# def sample_creater(trace_list, count, time_steps, label_card, label_size = 1, offset = 0):
+# 	if count + time_steps + label_size + offset > len(trace_list):
+# 		raise ValueError("trace_list is too short."
+# 						 "\tcount = " + str(count) +
+# 						 "\tlabel_size = " + str(label_size) +
+# 						 "\tlen(trace_list) = " + str(len(trace_list)) +
+# 						 "\toffset = " + str(offset))
+# 	if time_steps >= len(trace_list):
+# 		raise ValueError("time_steps is larger than trace_list.\ntrace_list is too short"
+# 			  "\ttime_steps = " + str(time_steps) +
+# 			  "\tlen(trace_list)" + str(len(trace_list)))
+#
+# 	example = np.zeros((count, time_steps, label_card))
+# 	label = np.zeros((count, time_steps, label_card))
+#
+# 	for l in range(count):
+# 		for j in range(time_steps):
+# 			''' TODO: find the correct index'''
+# 			task_num = trace_list[l + j]
+# 			example[l][j][task_num] = 1
+# 			for k in range(1, label_size + 1):
+# 				''' TODO: find the correct index'''
+# 				task_num = trace_list[l + j + k + offset]
+# 				label[l][j][task_num] = 1
+# 	return example, label
+#
+# '''
+# 	requires the length of the trace_list to be divisible by count.
+# '''
+# def sample_creater_disjoint(trace_list, count, time_steps, label_card, label_size = 1, offset = 0):
+#
+# 	example_len = (label_size + time_steps)
+#
+# 	example = np.zeros((count, time_steps, label_card))
+# 	label = np.zeros((count, time_steps, label_card))
+#
+# 	for i in range(count):
+# 		for j in range(time_steps):
+# 			task_num = trace_list[i*(example_len) + j]
+# 			example[i][j][task_num] = 1
+# 			for k in range(1,1+label_size):
+# 				label_num = trace_list[i*(example_len) + j + k + offset]
+# 				label[i][j][label_num] = 1
+#
+# 	return example, label
+
+# ''' DEBUG THIS'''
+# def generate_folds(folds, trace_list, training_ratio, time_steps, label_card, label_size=1, offset = 0):
+# 	trace_folds = []
+# 	fold_size = math.floor(len(trace_list)/folds)
+# 	for i in range(folds):
+# 		trace_folds.append(trace_list[fold_size * i:fold_size*(i+1)])
+#
+# 	result = []
+# 	for i in range(folds):
+# 		train, _ =list_to_array(trace_folds[i], 1, 100, 16)
+# 		result.append(train)
+#
+# 	return result
+
+def generate_folds2(folds, data_pair):
+	example_list = data_pair[0]
+	label_list = data_pair[1]
+
+	if len(example_list) != len(label_list):
+		raise ValueError("expected example and label to have same number of samples")
+
+	fold_size = math.floor(len(example_list)/folds)
+
+	time_steps = example_list[0].shape[0]
+	label_card = example_list[0].shape[1]
+	label_size = label_list[0].shape[0]
+
+
+	result = []
+	for i in range(folds):
+		example_dataset = np.zeros((fold_size, time_steps, label_card))
+		example_fold = example_list[i*fold_size:(i+1)*fold_size]
+		for j in range(len(example_fold)):
+			example_dataset[j]=example_fold[j]
+
+		label_dataset = np.zeros((fold_size, label_size, label_card))
+		label_fold = label_list[i * fold_size : (i + 1) * fold_size]
+		for j in range(len(label_fold)):
+			label_dataset[j]=label_fold[j]
+
+		result.append((example_dataset, label_dataset))
+	return result
+
+def index_of_label(vec):
+	for i in range(len(vec)):
+		if vec[i] == 1:
+			return i
+	return -1
+
+''' currently only checks correctly when label_size = 1'''
+def validate_dataset(example_dataset, label_dataset, offset=0):
+	label_size = label_dataset[0].shape[0]
+	print(label_size)
+	for i in range(len(example_dataset)-(offset + label_size)):
+		for j in range(label_size):
+			index = index_of_label(label_dataset[i][j])
+			if example_dataset[i+j+1+offset][-1][index] != 1:
+				print("ERROR")
+				print("label_vec: " + str(label_dataset[i][j]))
+				print("example_vec: " + str(example_dataset[i+j+1+offset][0][index]))
+				print("i: " + str(i) + " j: " + str(j) + " ")
+				print("label should be " + str(index))
+				print("training task is " + str(example_dataset[i+j+1+offset][0][index]))
+
+				return -1
+	return 0
+
+
+
+
+
+
+res = generate_folds2(5,list_to_example(text_to_list('dataset_1.txt'),16,100,2))
+validate_dataset(res[0][0],res[0][1],0)
--- guesser.py
+++ guesser.py
@@ -1,9 +1,28 @@
 from keras.models import Sequential
 from keras.layers import Dense
+from keras.layers import Dropout
 from keras.layers import LSTM
+import matplotlib.pyplot as plt
+import numpy as np
 import math
 
 import converter as cvt
+
+def score_display(fold_scores):
+    fold_count = len(fold_scores)
+    avg = []
+    for i in range(fold_count):
+        print("fold " + str(i) + ":", end="")
+        fold_avg = []
+        for j in range(fold_count-1):
+            fold_avg.append(fold_scores[i][j][1])
+            print("\t"+str(fold_scores[i][j][1]), end="")
+        avg.append(np.mean(fold_avg))
+        print("\tfold_avg: "+str(avg[i]))
+    total_avg = np.mean(avg)
+    print("total_avg: " + str(total_avg))
+    return total_avg
+
 
 def create_model(cell_count, shape, stateful, batch, output_dim):
     model = Sequential()
@@ -12,26 +31,47 @@ def create_model(cell_count, shape, stateful, batch, output_dim):
 			  batch_size=batch,
               stateful=stateful,
 			  return_sequences=True))
-    model.add(Dense(cell_count, activation='relu'))
-    model.add(LSTM(cell_count,
-                   input_shape=shape,
-                   batch_size=batch,
-                   stateful=stateful,
-                   return_sequences=True))
+    # model.add(Dense(cell_count, activation='relu'))
+    # # model.add(Dropout(0.3))
+    # model.add(LSTM(cell_count,
+    #                input_shape=(cell_count,output_dim),
+    #                batch_size=batch,
+    #                stateful=stateful,
+    #                return_sequences=True))
     model.add(Dense(output_dim, activation='relu'))
-    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    model.compile(loss='poisson', optimizer='adam', metrics=['accuracy'])
     return model
 
-raw_data = cvt.text_to_list('dataset_1.txt')
+def run(filename, fold_count, n, label_card, factor, validation_frac):
+    # raw_data = cvt.text_to_list(filename)
 
-(train_x, train_y), (test_x,test_y) = cvt.list_to_array(raw_data[:9999], 0.8, 100, 16)
-model = create_model(100, (100, 16), True, 1, 16)
+    # (train_x, train_y), (test_x,test_y) = cvt.list_to_array(raw_data[:9999], 0.8, 100, 16)
 
-print(train_x.shape)
+    # folds = cvt.generate_folds(fold_count, raw_data[:math.floor(len(raw_data)/factor)], validation_frac, n, label_card)
+    pure_raw_data = cvt.text_to_list(filename)
+    raw_data = cvt.list_to_example(pure_raw_data,label_card,n)
+    folds = cvt.generate_folds2(5,raw_data)
+    print(folds[0][1][0].shape)
+    fold_scores = []
 
-model.fit(train_x, train_y, epochs=1, batch_size=1)
+    for i in range(fold_count):
+        model = create_model(50, (n, label_card), True, 1, label_card)
+        train = folds[i]
+        fit_history = model.fit(train[0], train[1], epochs=5, batch_size=1, verbose=1)
 
-scores = model.evaluate(test_x, test_y, batch_size=1)
-print('')
-print(scores)
-print("Accuracy: %.2f%%" % (scores[1]*100))
+        score_list = []
+        for  j in range(fold_count):
+            if i == j:
+                continue
+            model.reset_states()
+            scores = model.evaluate(folds[j][0], folds[j][1], batch_size=1, verbose=0)
+            score_list.append(scores)
+        fold_scores.append(score_list)
+
+    score_display(fold_scores)
+
+dataset = {'dataset_det_1.txt', 'dataset_det_1.txt'}
+set_n ={50,75,100,150,200,300}
+
+
+run('dataset_new_det_1.txt',5,1,16,2,0)
