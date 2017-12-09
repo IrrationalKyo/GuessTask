13986af039ace4e66650cb492d3c1a772a35202d
Kyo Kim <kyokim751@gmail.com>
2017-12-09 15:52:18 -0600

    created chunk function

--- converter.py
+++ converter.py
@@ -43,105 +43,26 @@ def list_to_example(trace_list, label_card, time_steps, label_size=1, offset=0):
 
 	return (list_of_examples, list_of_labels)
 
+def chunk_examples(examples, labels, start_index, end_index):
+	if len(examples) != len(labels):
+		raise ValueError("length of examples and labels do not match")
 
+	chunk_size = end_index-start_index
 
-'''
-	converts the task list to numpy of shape (data_size, feature_size)
-	
-	label_card := cardinality of the label set
-	label_size := number of slot predictions
-	offset := number of slots ahead of the next slot
-'''
-# def list_to_array(trace_list, training_ratio, time_steps, label_card, label_size=1, offset = 0):
-# 	# data_size := total number of data points that can be generated
-# 	data_size = len(trace_list) - (time_steps + label_size)
-# 	# training_size := number of training samples
-# 	training_size = int(data_size * training_ratio)
-# 	# test_size := number of test samples
-# 	test_size = data_size - training_size
-#
-# 	# training_examples = np.zeros((training_size, time_steps, label_card))
-# 	# training_labels = np.zeros((training_size, time_steps, label_card))
-#
-# 	# test_examples = np.zeros((test_size, time_steps, label_card))
-# 	# test_labels = np.zeros((test_size, time_steps, label_card))
-#
-# 	training_examples, training_labels = sample_creater(trace_list[:training_size + time_steps +label_size],
-# 														training_size,
-# 														time_steps,
-# 														label_card,
-# 														offset=offset)
-# 	test_examples, test_labels = sample_creater(trace_list[training_size:],
-# 												test_size,
-# 												time_steps,
-# 												label_card,
-# 												offset=offset)
-#
-# 	return (training_examples, training_labels), (test_examples, test_labels)
+	time_steps = examples[0].shape[0]
+	label_card = examples[0].shape[1]
+	label_size = labels[0].shape[0]
 
+	example_dataset = np.zeros((chunk_size, time_steps, label_card))
+	example_chunk = examples[start_index:end_index]
+	label_dataset = np.zeros((chunk_size, label_size, label_card))
+	label_fold = labels[start_index:end_index]
 
-# '''
-# 	Creates samples so each new example advances by 1 timestep (1 new label example)
-# '''
-# def sample_creater(trace_list, count, time_steps, label_card, label_size = 1, offset = 0):
-# 	if count + time_steps + label_size + offset > len(trace_list):
-# 		raise ValueError("trace_list is too short."
-# 						 "\tcount = " + str(count) +
-# 						 "\tlabel_size = " + str(label_size) +
-# 						 "\tlen(trace_list) = " + str(len(trace_list)) +
-# 						 "\toffset = " + str(offset))
-# 	if time_steps >= len(trace_list):
-# 		raise ValueError("time_steps is larger than trace_list.\ntrace_list is too short"
-# 			  "\ttime_steps = " + str(time_steps) +
-# 			  "\tlen(trace_list)" + str(len(trace_list)))
-#
-# 	example = np.zeros((count, time_steps, label_card))
-# 	label = np.zeros((count, time_steps, label_card))
-#
-# 	for l in range(count):
-# 		for j in range(time_steps):
-# 			''' TODO: find the correct index'''
-# 			task_num = trace_list[l + j]
-# 			example[l][j][task_num] = 1
-# 			for k in range(1, label_size + 1):
-# 				''' TODO: find the correct index'''
-# 				task_num = trace_list[l + j + k + offset]
-# 				label[l][j][task_num] = 1
-# 	return example, label
-#
-# '''
-# 	requires the length of the trace_list to be divisible by count.
-# '''
-# def sample_creater_disjoint(trace_list, count, time_steps, label_card, label_size = 1, offset = 0):
-#
-# 	example_len = (label_size + time_steps)
-#
-# 	example = np.zeros((count, time_steps, label_card))
-# 	label = np.zeros((count, time_steps, label_card))
-#
-# 	for i in range(count):
-# 		for j in range(time_steps):
-# 			task_num = trace_list[i*(example_len) + j]
-# 			example[i][j][task_num] = 1
-# 			for k in range(1,1+label_size):
-# 				label_num = trace_list[i*(example_len) + j + k + offset]
-# 				label[i][j][label_num] = 1
-#
-# 	return example, label
+	for j in range(len(label_fold)):
+		label_dataset[j]=label_fold[j]
+		example_dataset[j] = example_chunk[j]
 
-# ''' DEBUG THIS'''
-# def generate_folds(folds, trace_list, training_ratio, time_steps, label_card, label_size=1, offset = 0):
-# 	trace_folds = []
-# 	fold_size = math.floor(len(trace_list)/folds)
-# 	for i in range(folds):
-# 		trace_folds.append(trace_list[fold_size * i:fold_size*(i+1)])
-#
-# 	result = []
-# 	for i in range(folds):
-# 		train, _ =list_to_array(trace_folds[i], 1, 100, 16)
-# 		result.append(train)
-#
-# 	return result
+	return (example_dataset, label_dataset)
 
 def generate_folds2(folds, data_pair):
 	example_list = data_pair[0]
@@ -197,9 +118,6 @@ def validate_dataset(example_dataset, label_dataset, offset=0):
 	return 0
 
 
-
-
-
-
-res = generate_folds2(5,list_to_example(text_to_list('dataset_1.txt'),16,100,2))
-validate_dataset(res[0][0],res[0][1],0)
+if __name__ == "__main__":
+	res = generate_folds2(5,list_to_example(text_to_list('dataset_1.txt'),16,100,2))
+	validate_dataset(res[0][0],res[0][1],0)
