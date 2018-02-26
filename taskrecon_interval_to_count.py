f45984f7391d721420bb91ca5aec66a451a5d67a
Kyo Kim <kyokim751@gmail.com>
2018-02-26 13:50:50 -0600

    Added trace to interval parsing
    Added trace to interval parsing

--- taskrecon_interval_to_count.py
+++ taskrecon_interval_to_count.py
@@ -1,13 +1,14 @@
 import random
 import math
 import numpy as np
-from keras.layers import Input, Dense,Flatten
-
+from keras.layers import Input, Convolution2D, MaxPooling2D, Dense, Dropout, Flatten
+from keras.models import Sequential
 import  keras.models as km
 
 
 from keras.utils import plot_model
-
+from os import listdir
+from os.path import isfile, join
 
 '''
     This python script creates a model to infer number of tasks from busy intervals.
@@ -58,19 +59,19 @@ class Converter:
         int_prev_task = string_trace[0]
         for i in range(1, len(string_trace)):
             # int_interval_duration += 1
-            int_current_task = string_trace[i]
-            int_change_flag = Converter.state_changed(int_current_task, int_prev_task)
-            if int_change_flag == -1:
+            int_current_task = 1 if string_trace[i] > 0 else 0
+            int_change_flag = int_current_task - int_prev_task
+            if int_change_flag > 0:
                 vect.append((0, int_interval_duration))
                 int_interval_duration = 0
-            elif int_change_flag == -2:
+            elif int_change_flag < 0:
                 vect.append((1, int_interval_duration))
                 int_interval_duration = 0
-            elif int_change_flag > 0:
-                # write interval
-                vect.append((1, int_interval_duration))
-                # write the context switch change.
-                int_interval_duration = 0
+            # elif int_change_flag > 0:
+            #     # write interval
+            #     vect.append((1, int_interval_duration))
+            #     # write the context switch change.
+            #     int_interval_duration = 0
             int_interval_duration += 1
             int_prev_task = int_current_task
 
@@ -142,6 +143,16 @@ class Converter:
 
         return  mat_train_x, mat_train_y, mat_test_x, mat_test_y
 
+    # takes a vector of traces with corresponding labels
+    # Inflates the data by convoluting window
+    @staticmethod
+    def data_prep(vec_o_vec_trace, int_window_len, int_conv_count, vec_labels, int_window_slide = None):
+        vec_o_tup_samples = []
+        for i in range(len(vec_o_vec_trace)):
+            vec_o_vec_convoluted_bunch = Converter.data_convolutor(Converter.vectorize(vec_o_vec_trace[i]), int_window_len , int_conv_count, int_window_slide)
+            vec_o_tup_samples += Converter.assign_label(vec_o_vec_convoluted_bunch, vec_labels[i])
+        return Converter.vec_o_tup_to_mat(vec_o_tup_samples)
+
 class Model:
 
     def __init__(self, int_cell_count):
@@ -149,41 +160,116 @@ class Model:
         self._model = None
         self.create_model()
 
-    def create_model(self):
-        return
+    # each row is a data point
+    # each column is a feature(i.e. single interval in this case)
+    def create_model(self, int_rows, int_columns):
+        self._model = Sequential()
+        self._model.add(Dense(100, input_shape=(int_rows, int_columns), activation='relu'))
+        self._model.add(Dense(50, activation='relu'))
+        self._model.add(Dense(25, activation='relu'))
+        self._model.add(Dense(1, activation='linear'))
+        self._model.compile(loss="mse", optimizer='adam', metrics=["accuracy"])
+
+
+def partition_train_test(data_dict, ratio):
+    train_x = []
+    train_y = []
+    test_x = []
+    test_y = []
+    remapped_x= []
+    remapped_y = []
+
+    for label, vec_data in data_dict.items():
+        data_len = len(vec_data)
+        index_list = np.random.perumation(data_len)
+        for i in range(math.floor(data_len * ratio)):
+            train_x.append(vec_data[index_list[i]])
+            train_y.append(label)
+        for i in range(math.floor(data_len * ratio), data_len):
+            test_x.append(vec_data[index_list[i]])
+            test_y.append(label)
+
+    index_list = np.random.permutation(len(train_x))
+    for i in range(len(train_x)):
+        remapped_x.append(train_x[index_list[i]])
+        remapped_y.append(train_y[index_list[i]])
+    train_x = remapped_x
+    train_y = remapped_y
+
+    remapped_x= []
+    remapped_y= []
+    index_list = np.random.permutation(len(test_x))
+    for i in range(len(test_x)):
+        remapped_x.append(test_x[index_list[i]])
+        remapped_y.append(test_y[index_list[i]])
+    test_x = remapped_x
+    test_y = remapped_y
+
+    return train_x, train_y, test_x, test_y
+
 
 
 
 if __name__ == "__main__":
-    int_input_length = 12
-    vec_datum1 = [0,0,0,0,1,1,1,2,1,2,0,0,1,1,1,1,1,1,0,1,1,0,1,1,2,2,2,2,2,0,0,0,0,1,2,1]
-    vec_datum2 = [0,1,1,0,1,1,2,2,2,2,2,0,0,0,0,1,2,1, 0,0,0,0,1,1,1,2,1,2,0,0,1,1,1,1,1,1]
-    vec_datum3 = [1, 2, 1, 1, 1, 0, 2, 2, 2, 2, 2, 1, 1, 1, 0, 1, 2, 1]
-
-    vec_data1 = Converter.assign_label(Converter.data_convolutor(Converter.vectorize(vec_datum1), int_input_length, 2), 2)
-    vec_data2 = Converter.assign_label(Converter.data_convolutor(Converter.vectorize(vec_datum2), int_input_length, 2), 2)
-    # vec_data1.append()
-
-    vec_data1 += vec_data2
-    train_x = Converter.vec_o_tup_to_mat(vec_data1)
+    # int_input_length = 4
+    # vec_datum1 = [0,0,0,0,1,1,1,2,1,2,0,0,1,1,1,1,1,1,0,1,1,0,1,1,2,2,2,2,2,0,0,0,0,1,2,1]
+    # vec_datum2 = [0,1,1,0,1,1,2,2,2,2,2,0,0,0,0,1,2,1, 0,0,0,0,1,1,1,2,1,2,0,0,1,1,1,1,1,1]
+    # vec_datum3 = [1, 2, 1, 1, 1, 0, 2, 2, 2, 2, 2, 1, 1, 1, 0, 1, 2, 1]
+    #
+    # vec_data1 = Converter.assign_label(Converter.data_convolutor(Converter.vectorize(vec_datum1), int_input_length, 2), 2)
+    # vec_data2 = Converter.assign_label(Converter.data_convolutor(Converter.vectorize(vec_datum2), int_input_length, 2), 2)
+    # # vec_data1.append()
+    #
+    # vec_data1 += vec_data2
+    # print(vec_data1)
+    # train_x = Converter.vec_o_tup_to_mat(vec_data1)
+    # print(train_x)
+    #
+    # print(Converter.data_prep([vec_datum1, vec_datum2], int_input_length, 2, [2,2]))
 
 
     # #
-    input_layer = Input(shape=(int_input_length, 2))
-    dense_layer_1 = Dense(math.floor(int_input_length/2), name="dense_1")(input_layer)
-    dense_layer_2 = Dense(math.floor(int_input_length/4), name="dense_2")(dense_layer_1)
-    # dense_layer_3 = Dense(math.floor(int_input_length/8))(dense_layer_2)
-    # dense_layer_4 = Dense(math.floor(int_input_length/16))(dense_layer_3)
-    flat_layer = Flatten()(dense_layer_2)
-    dense_layer_5 = Dense(1, name="dense_3")(flat_layer)
+    # input_layer = Input(shape=(int_input_length, 2))
+    # dense_layer_1 = Dense(math.floor(int_input_length/2), name="dense_1")(input_layer)
+    # dense_layer_2 = Dense(math.floor(int_input_length/4), name="dense_2")(dense_layer_1)
+    # # dense_layer_3 = Dense(math.floor(int_input_length/8))(dense_layer_2)
+    # # dense_layer_4 = Dense(math.floor(int_input_length/16))(dense_layer_3)
+    # flat_layer = Flatten()(dense_layer_2)
+    # dense_layer_5 = Dense(1, name="dense_3")(flat_layer)
+    #
+    #
+    # model = km.Model(inputs=input_layer, outputs=dense_layer_5)
+    # model.compile(loss="mean_squared_error", optimizer='adam', metrics=['accuracy'])
+    #
+    # plot_model(model, to_file='model.png')
+    #
+    # model.fit(train_x[0], train_x[1])
+    stop = 0
+    onlyfiles = [f for f in listdir("./data/") if isfile(join("./data/", f))]
+
+    data_dict = {}
+
+    for filename in onlyfiles:
+        label = int(filename.split("rep")[0].split("size")[1])
+        rep_count = int(filename.split("rep")[1].split(".")[0])
+
+        if label not in data_dict:
+            data_dict[label] = []
+
+        with open("./data/" + filename) as file:
+            data = file.read()
+            parsed_trace = [int(x) for x in data.split(", ")]
+            data_dict[label].append(parsed_trace)
+            file.close()
 
 
-    model = km.Model(inputs=input_layer, outputs=dense_layer_5)
-    model.compile(loss="mean_squared_error", optimizer='adam', metrics=['accuracy'])
+    train_x, train_y, test_x, test_y = partition_train_test(data_dict, 0.75)
+    train_x, train_y = Converter.data_prep(train_x, 5000, 2, train_y)
+    test_x, test_y = Converter.data_prep(test_x, 5000, 2, test_y)
 
-    plot_model(model, to_file='model.png')
-
-    model.fit(train_x[0], train_x[1])
+    print(test_x)
 
 
+    # mod = Model(100)
+    # model = mod._model
 
